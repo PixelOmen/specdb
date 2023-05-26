de35a2d901f7dfebac54e6ab92ecdaf600b4db34
Eman <eacosta@roundabout.com>
2023-05-26 13:10:33 -0700

    Added delete_client util (deletes child specs)

--- utils.py
+++ utils.py
@@ -6,6 +6,14 @@ from .schema import Spec, TempSpec, Client
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
+def delete_client(client_name: str, session: "Session") -> None:
+    client = session.query(Client).filter(Client.name == client_name).first()
+    if client is None:
+        return
+    for spec in client.specs:
+        session.delete(spec)
+    session.delete(client)
+
 def spec_to_temp(transferfunc: Callable[[Spec], TempSpec], session: "Session") -> None:
     TempSpec.__table__.drop(ENGINE, checkfirst=True)
     TempSpec.__table__.create(ENGINE)
