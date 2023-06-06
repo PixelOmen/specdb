b421507472c9e143b82cdc1834daa4e26255a3a2
Eman <eacosta@roundabout.com>
2023-06-06 13:27:53 -0700

    Fixed order of init_db so Client table is created first

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
 def init_db() -> None:
-    Spec.__table__.create(ENGINE, checkfirst=True)
     Client.__table__.create(ENGINE, checkfirst=True)
+    Spec.__table__.create(ENGINE, checkfirst=True)
 
 def delete_client(client_name: str, session: "Session") -> None:
     client = session.query(Client).filter(Client.name == client_name).first()
